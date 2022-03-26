ade15a9355331992e2aa4a75ce90fc6cae5652ba
chiajen-john <83084166+chiajen-john@users.noreply.github.com>
2022-03-26 18:54:39 -0400

    Update adder_subtractor.v

--- simple_ALU/adder_subtractor.v
+++ simple_ALU/adder_subtractor.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 
-// Design Name: 
-// Module Name: adder_subtractor
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module adder_subtractor(A, B, CIN, M, SUM, CAR, V);
     input [3:0]A;
