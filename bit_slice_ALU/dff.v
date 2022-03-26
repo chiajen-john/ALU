bc304bc0511f9a15fe01826038cc10ed3e2e3649
chiajen-john <83084166+chiajen-john@users.noreply.github.com>
2022-03-26 18:53:51 -0400

    Update dff.v

--- bit_slice_ALU/dff.v
+++ bit_slice_ALU/dff.v
@@ -1,29 +1,11 @@
 `timescale 1ns / 1ns
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:
-// Design Name: 
-// Module Name:    dff 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
 
 module dff(q, d, clk);
   input		d, clk; 
   output reg	q; 
 
-  always @(posedge clk)
+  always @(posedge clk) begin
     q=d;
+  end
   
 endmodule
