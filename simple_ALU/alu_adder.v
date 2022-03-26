fd0c0421c21bda97ecc434303b843540c88f2cef
chiajen-john <83084166+chiajen-john@users.noreply.github.com>
2022-03-26 18:57:04 -0400

    Update alu_adder.v

--- simple_ALU/alu_adder.v
+++ simple_ALU/alu_adder.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/04/2021 10:11:50 AM
-// Design Name: 
-// Module Name: alu_adder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// This is a carry-look-ahead adder
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module alu_adder(A, B, SUM);
     input [3:0]A,B;
