a810e23cb13045260075f1b9d1ae7903e0b10689
chiajen-john <83084166+chiajen-john@users.noreply.github.com>
2022-03-26 18:56:53 -0400

    Update alu_concate.v

--- simple_ALU/alu_concate.v
+++ simple_ALU/alu_concate.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/04/2021 11:17:02 AM
-// Design Name: 
-// Module Name: alu_concate
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module alu_concate(A,B,Y);
     input [3:0]A,B;
