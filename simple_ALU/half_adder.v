eedc5077689e5af63e9b730c99b65387e8418c0d
chiajen-john <83084166+chiajen-john@users.noreply.github.com>
2022-03-26 18:55:31 -0400

    Update half_adder.v

--- simple_ALU/half_adder.v
+++ simple_ALU/half_adder.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09/27/2021 10:43:33 AM
-// Design Name: 
-// Module Name: half_adder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module half_adder(
     input A,
