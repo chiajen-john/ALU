2699222e7cce8412db70f22fa995799370ca33c3
chiajen-john <83084166+chiajen-john@users.noreply.github.com>
2022-03-26 18:56:20 -0400

    Update alu_multiplier.v

--- simple_ALU/alu_multiplier.v
+++ simple_ALU/alu_multiplier.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/04/2021 11:16:24 AM
-// Design Name: 
-// Module Name: alu_multiplier
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module alu_multiplier(A,B,P);
     input [3:0]A,B;
