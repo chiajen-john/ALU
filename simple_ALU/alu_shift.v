011867ab7b85b390dfad06c18a78ad4f5e587e35
chiajen-john <83084166+chiajen-john@users.noreply.github.com>
2022-03-26 18:56:04 -0400

    Update alu_shift.v

--- simple_ALU/alu_shift.v
+++ simple_ALU/alu_shift.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/04/2021 11:40:37 AM
-// Design Name: 
-// Module Name: alu_shift
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module alu_shift(A,B,Y);
     input [3:0]A,B;
