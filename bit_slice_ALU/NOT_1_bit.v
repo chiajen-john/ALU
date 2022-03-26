927972bb7a58d0631226a71de1af37c3450f1479
chiajen-john <83084166+chiajen-john@users.noreply.github.com>
2022-03-26 18:52:59 -0400

    Update NOT_1_bit.v

--- bit_slice_ALU/NOT_1_bit.v
+++ bit_slice_ALU/NOT_1_bit.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/15/2022 11:36:22 AM
-// Design Name: 
-// Module Name: NOT_1_bit
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module NOT_1_bit(out, a, b);
     input a, b;
