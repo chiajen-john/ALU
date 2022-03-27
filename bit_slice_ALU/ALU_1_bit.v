2f23e00be59aadea30a16114fe5372b393e974d6
chiajen-john <83084166+chiajen-john@users.noreply.github.com>
2022-03-26 20:06:16 -0400

    Update ALU_1_bit.v

--- bit_slice_ALU/ALU_1_bit.v
+++ bit_slice_ALU/ALU_1_bit.v
@@ -8,16 +8,18 @@ module ALU_1_bit(out, c_out, a, b, select, c_in);
     wire new_b, NOT_b, out_1, out_2, out_3, out_4, out_5;
     
     assign new_b = (select==3'b101)?NOT_b:b; // mux
-    
     not g0 (NOT_b, b);
-    FA_1_bit g1 (.c_out(c_out), 
-                 .sum(out_1), 
-                 .a(a), .b(new_b), 
-                 .c_in(c_in)); // add or subtract 
-    MOV_1_bit g2 (out_2, a, b); // move a to out
-    NOT_1_bit g3 (out_3, a, b); // not a
+  
+    MOV_1_bit g2 (out_2, a, b); 
+    NOT_1_bit g3 (out_3, a, b);
     NAND_1_bit g4 (out_4, a, b);
     NOR_1_bit g5 (out_5, a, b);
+    
+    FA_1_bit g1 (
+        .c_out(c_out), 
+        .sum(out_1), 
+        .a(a), .b(new_b), 
+        .c_in(c_in)); // add or subtract 
     
     always @(*) begin
         case (select)
