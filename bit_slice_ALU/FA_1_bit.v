0344b9ade8ff52b4b478a5d2b05067609113c44f
chiajen-john <83084166+chiajen-john@users.noreply.github.com>
2022-03-29 15:49:04 -0400

    Update FA_1_bit.v

--- bit_slice_ALU/FA_1_bit.v
+++ bit_slice_ALU/FA_1_bit.v
@@ -5,10 +5,10 @@ module FA_1_bit(c_out, sum, a, b, c_in);
   output c_out, sum; 
   wire xor_1, and_1, and_2;
 	
-	xor G1(xor_1, a, b);
-	xor G2(sum, xor_1, c_in);
-	and G3(and_1, xor_1, c_in);
-	and G4(and_2, a, b);
-	or G5(c_out, and_1, and_2);
+  xor G1(xor_1, a, b);
+  xor G2(sum, xor_1, c_in);
+  and G3(and_1, xor_1, c_in);
+  and G4(and_2, a, b);
+  or G5(c_out, and_1, and_2);
 
 endmodule
