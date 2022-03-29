97f9426b04fa3e9baf268dc74855b3c891940b98
chiajen-john <83084166+chiajen-john@users.noreply.github.com>
2022-03-29 15:48:17 -0400

    Update FA_1_bit.v

--- bit_slice_ALU/FA_1_bit.v
+++ bit_slice_ALU/FA_1_bit.v
@@ -3,24 +3,12 @@
 module FA_1_bit(c_out, sum, a, b, c_in);
   input	a, b, c_in; 
   output c_out, sum; 
-  wire not_a, not_b, not_c;
-  wire and1_out, and2_out, and3_out, and4_out;
-  wire and5_out, and6_out, and7_out, and8_out;
-
-  not	not1	(not_a, a);
-  not	not2	(not_b, b);
-  not	not3	(not_c, c_in);
-
-  and	and1	(and1_out, a, not_b, not_c);
-  and	and2	(and2_out, not_a, b, not_c);
-  and	and3	(and3_out, not_a, not_b, c_in);
-  and	and4	(and4_out, a, b, c_in);
-  or	or1	(sum, and1_out, and2_out, and3_out, and4_out);
-						
-  and	and5	(and5_out, a, b);
-  and	and6	(and6_out, b, c_in);
-  and	and7	(and7_out, a, c_in);
-  and	and8	(and8_out, a, b, c_in);
-  or	or2	(c_out, and5_out, and6_out, and7_out, and8_out);
+	wire xor_1, and_1, and_2;
+	
+	xor G1(xor_1, a, b);
+	xor G2(sum, xor_1, c_in);
+	and G3(and_1, xor_1, c_in);
+	and G4(and_2, a, b);
+	or G5(c_out, and_1, and_2);
 
 endmodule
